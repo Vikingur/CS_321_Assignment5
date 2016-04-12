a66ecd1cfb930950c8583b354d8d50c630ee73f6
James A Casebeer <jcasebee@zeus-1.vse.gmu.edu>
2016-04-12 17:21:06 -0400

    jack: BankSystem's actual code is complete. Will convert to pseudocode soon

--- BankSystem.java
+++ BankSystem.java
@@ -1,55 +1,86 @@
+import java.util.*;
+import java.lang.*;
+
+// Proxy class of a customer for testing purposes
+// TODO Remove when Customer exists
+class Customer
+{
+	public Customer()
+	{
+		
+	}
+	public String getCCType()
+	{
+		return "MasterCard";
+	}
+	public String getCCNumber()
+	{
+		return "12342345 2835935 1  324";
+	}
+	public String getCCExpiration()
+	{
+		return "10/2015";
+	}
+}
+
+// Proxy class of the calender for testing purposes
+// TODO Remove when Calender exists
+class Calender
+{
+    public static int getYear()
+    {
+        return 2016;
+    }
+    public static int getMonth()
+    {
+        return 1;
+    }
+}
+
 public class BankSystem
 {
-	public class Reservation
+	public static boolean validateCard(Customer customer)
 	{
-		public Reservation()
-		{
-			
-		}
-	}
-	
-	public class Customer
-	{
-		public Customer()
-		{
-			
-		}
-		public String getCCType()
-		{
-			return "MasterCard";
-		}
-		public String getCCNumber()
-		{
-			return "12342345 2835935 1  324";
-		}
-		public String getCCExpiration()
-		{
-			return "10/2015";
-		}
-	}
-	public static boolean validateCard(Customer customer, Reservation res)
-	{
+        // get the customer's credit card info, converting the number into an
+        // integer
 		String ccType = customer.getCCType();
-		long ccNumber = Integer.parseLong(customer.getCCNumber().replaceAll("[^a-zA-z]",""));
-		String[] ccExpiration = customer.getCCExpiration().split("/");
-		int expirationMonth = Integer.parseInt(ccExpiration[0]);
+		long ccNumber = Long.parseLong(customer.getCCNumber().replaceAll("[^0-9]",""));
+        
+        // split the date on / and - characters
+        // Assuming the format MONTH/YEAR
+		String[] ccExpiration = customer.getCCExpiration().split("(/|-)");
+		
+        // grab the Month/Year and convert to integers
+        int expirationMonth = Integer.parseInt(ccExpiration[0]);
 		int expirationYear = Integer.parseInt(ccExpiration[1]);
 		
-		system.out.println("Expiration Month:",expirationMonth);
-		
-		return false;
-		
+        // get the current month and year for comparison
+        int currentMonth = Calender.getMonth();
+        int currentYear = Calender.getYear();
+        
+        // compare the current month/year to the expiration month/year
+		if (currentYear*100+currentMonth<expirationYear*100+expirationMonth)
+        {
+            // if the card is not expired. Send the number and type to
+            // the external bank system for validation
+            return queryExternalBank(ccNumber,ccType);
+        }
+        return false;
 	}
 	
 	private static boolean queryExternalBank(long ccNumber, String ccType)
 	{
+        // This is the function that "asks the external bank if the card 
+        // is valid" we're just going to assume that any card that is 
+        // not expired is valid for simplicities sake
 		return true;
 	}
-	
-	public void main(String[] args)
+    
+    // test method.
+    // TODO Remove when integrating BankSystem
+	public static void main(String[] args)
 	{
 		Customer c = new Customer();
-		Reservation r = new Reservation();
-		validateCard(c,r);
+		System.out.println(""+validateCard(c));
 	}
 }
