b87122418b592ad1f60cc9f9cb3fe61489861f24
sjammi <sushenajammi@gmail.com>
2016-04-13 19:52:43 -0400

    Added files via upload

---
+++ Calendar_Date.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+
+
+class Calendar {
+
+	ArrayList<Date> days;
+	Date currentDay;
+
+
+	public Calendar() {
+
+		days = new ArrayList<Date>();
+	}
+
+	public void buildCalendar() {
+
+		for(int i=1; i<=Framework.NUM_DAYS; i++) {
+			Date day = new Date();
+			days.add(day);
+		}
+	}
+
+	public void addReservation(int id, int date) {
+		Date day = days.get(date);
+		day.addReservation(id);
+	}
+}
+
+
+
+class Date {
+
+	ArrayList<Integer> reservationIDs;
+	int numReservations;
+
+	public Date() {
+		reservationIDs = new ArrayList<Integer>();
+		numReservations = 0;
+	}
+
+	public void addReservation(int id) {
+		reservationIDs.add(id);
+		numReservations++;
+	}
+
+	public getNumReservations() {	//For the Manager Report.
+		return this.numReservations;
+	}
+
+	public ArrayList<Integer> getIDs() {
+		return this.reservationIDs;
+	}
+}
---
+++ managerReport.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+/* FINAL REPORT SAYS:
+	Report gets info for the CURRENT DAY
+	and returns: number of reservations,
+	number of reserved/unreserved rooms,
+	occupancy rate, and revenue generated
+	for the day.
+*/
+
+//Called with input of Calendar.currentDay
+
+class managerReport {
+
+	Date currentDay;
+	int reservations = 0;
+	int reserved = 0;
+	int unreserved = 0;
+	int occupancyRate = 0;
+	int revenue = 0;
+	Arraylist<Integer> reservations;
+
+	public managerReport(Date day) {
+
+		this.currentDay = day;
+		buildReport();
+	}
+
+	private void getRevenue() {
+		this.reservations = currentDay.getIDs();
+		Iterator iterate = reservations.iterator();
+		Reservation reserve;
+		int singles = 0;
+		int doubles = 0;
+
+		while(iterate.hasNext()) {
+			reserve = iterate.next();
+			if(reserve.getRoomType == 1)
+				singles++;
+			else
+				doubles++;
+		}
+
+		this.revenue = (Framework.SINGLE_RATE * singles) + (Framework.DOUBLE_RATE * doubles);
+	}
+
+	private void getOccupancy() {
+		this.reservations = currentDay.getIDs();
+		Iterator iterate = reservations.iterator();
+		Reservation reserve;
+		int occupantsByRoom;
+
+		while(iterate.hasNext()) {
+			reserve = iterate.next();
+			occupantsByRoom = reserve.getNumOccupants();
+			this.occupancyRate += occupantsByRoom;
+		}
+	}
+
+	private void bulidReport() {
+
+		this.reservations = currentDay.getNumReservations();
+		this.unreserved = (Framework.NUM_SINGLE_ROOMS) + (Framework.NUM_DOUBLE_ROOMS) - this.reservations;
+		this.reserved = this.reservations
+		getOccupancy();
+		getRevenue();
+
+	}
+}
