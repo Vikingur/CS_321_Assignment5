f5916a0b8ebe70c29051787d851044b3f98e75ac
jordanNewton8 <jordan.m.newton@gmail.com>
2016-04-14 13:13:11 -0400

    Calendar and Date
    Separated Calendar and Date into two separate files and finished both
    classes. Added additional variables to make it easier to communicate
    with the signalTimer and processReservations classes that are coming.

---
+++ Calendar.java
@@ -0,0 +1,38 @@
+import java.util.*
+import java.util.ArrayList;
+
+public class Calendar {
+	//Class variables
+	ArrayList<Date> dates;
+	Date currentDate;
+
+	//Class constructor
+	public Calendar() {
+		dates = new ArrayList<Date>();
+		buildCalendar();
+		currentDate = dates.get(0);
+	}
+
+	//Constructor helper method (to populate with dates)
+	public void buildCalendar() {
+		for(int i=1; i<=Framework.NUM_DAYS; i++) {
+			dates.add(new Date(1,i));
+		}
+	}
+
+	//Class setters
+	public void setCurrentDate(Date inDate){
+		currentDate = inDate;
+	}
+	
+	//Class getters
+	public Date getCurrentDate(){
+		return currentDate;
+	}
+	public ArrayList<Date> getDates(){
+		return dates;
+	}
+	public Date getDate(int day){
+		return dates.get(day-1);
+	}	
+}
---
+++ Date.java
@@ -0,0 +1,49 @@
+import java.util.*
+import java.util.ArrayList;
+
+public class Date{
+	//Class variables
+	int day;
+	int month;
+	int numReservations;
+	ArrayList<Integer> reservationIDs;
+
+	//Class constructor
+	public Date(int inMonth, int inDay) {
+		reservationIDs = new ArrayList<Integer>();
+		numReservations = 0;
+		day = inDay;
+		month = inMonth;
+	}
+
+	//Class setters
+	public void setDay(int inDay){
+		day = inDay;
+	}
+	public void setMonth(int inMonth){
+		month = inMonth;
+	}
+	public void setNumReservations(int inNum){
+		numReservations = inNum;
+	}
+	
+	//Class getters
+	public int getDay(){
+		return day;
+	}
+	public int getMonth(){
+		return month;
+	}
+	public int getNumReservations(){
+		return numReservations;
+	}
+	public ArrayList<Integer> getReservationIDs(){
+		return reservationIDs;
+	}
+	
+	//Reservation list methods
+	public void addReservation(int reservationId) {
+		reservationIDs.add(reservationId);
+		numReservations++;
+	}
+}
