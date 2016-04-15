2ac2af89bbd5648e2d9285ad75cdaef4ded73bc6
jordanNewton8 <jordan.m.newton@gmail.com>
2016-04-14 21:40:40 -0400

    Added static class ProcessReservations to Calendar
    Added the static class ProcessReservations to Calendar which emulates
    the corresponding businesslogic in our interface.

--- Calendar.java
+++ Calendar.java
@@ -34,5 +34,31 @@ public class Calendar {
 	}
 	public Date getDate(int day){
 		return dates.get(day-1);
-	}	
+	}
+	
+   //Method that iterates across all reservations tied to a given day and changes their statuses appropriately
+	public static class processReservations{
+		public static void checkReservations(Calendar systemCalendar){
+			Date currentDate = systemCalendar.getCurrentDate();
+			ArrayList<Integer> reservationIDs = currentDate.getReservationIDs();
+			Reservation currentReservation;
+			//Iterate across all reservations associated with the current date to flag as late, must-pay, cancel, etc.
+			for(int i = 0; i < currentDate.getNumReservations(); i++){
+				currentReservation = Framework.getReservationByID(reservationIDs.get(i));
+				//For reservations who have not been checked-in, checked-out, or not flagged as must-pay
+				if (currentReservation.getStatus() == 1){
+					//For reservations whose check-in dates have passed
+					if(currentDate.getDay() >= currentReservation.getStartDate()){
+						//Flag guaranteed reservations as 'must-pay' (status code = 5)
+						if(currentReservation.getGuaranteed() == 1){
+							currentReservation.setStatus(5);
+                  }
+						//Flag not-guaranteed reservations as 'no-show' (status code = 4)
+						else
+							currentReservation.setStatus(4);
+					}
+				}
+			}
+		}
+	}
 }
