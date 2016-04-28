deed75bd54e8ab1afb38abef81ad7bde87a67cac
sjammi <sushenajammi@gmail.com>
2016-04-28 19:17:15 -0400

    Sushi: Added CC Validation.

--- Reservation.java
+++ Reservation.java
@@ -73,9 +73,15 @@ public class Reservation{
       reservation.setGuaranteed(Integer.parseInt(inReservationInfo[7]));
       //If reservation is guaranteed, read and store credit card info to customer
       if(Integer.parseInt(inReservationInfo[7]) == 1){
+      	if(validateCard(inReservationInfo[8], inReservationInfo[9], inReservationInfo[10])) {
          customer.setCCType(inReservationInfo[8]);
          customer.setCCExpiration(inReservationInfo[9]);
          customer.setCCNumber(inReservationInfo[10]);
+      	}
+      	else {
+      		return "Given credit card not valid.";
+      	 }
+      	}
       }
       int CID = Framework.storeCustomer(customer);
       reservation.setCustomerID(CID);
